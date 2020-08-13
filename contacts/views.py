cdd22d9fa30e2e3b4087c985f045f46ef40396ca
Shreya <shreyarpate@gmail.com>
2020-08-13 11:37:57 +0530

    Initial commit

---
+++ contacts/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Contact
+
+def contact(request):
+    if request.method == 'POST':
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        realtor_email = request.POST['realtor_email']
+
+        contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id )
+
+        contact.save()
+
+        message.success(request, 'Your request has been submitted , a realtor should get back to you soon')
+        return redirect('/listing/'+listing_id)
