a8663043a1c908ce69058794fc204b36666e9c6a
shreyapate <shreyarpate@gmail.com>
2020-08-13 18:13:35 +0530

    settings file

--- btre/settings.py
+++ btre/settings.py
@@ -126,3 +126,8 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
